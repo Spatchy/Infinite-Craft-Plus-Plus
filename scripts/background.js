3af30d16f603e85d22f9e4f896703e0d2402d503
Spatchy <spatchy@spatchy.net>
2024-02-17 20:28:03 +0000

    cleanup

--- scripts/background.js
+++ scripts/background.js
@@ -3,7 +3,7 @@ chrome.webRequest.onCompleted.addListener(
         if (details.method === "GET" && details.url.startsWith("https://neal.fun/api/infinite-craft/pair")) {
             chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
                 chrome.tabs.sendMessage(tabs[0].id, { action: "responseReceived", response: details })
-            });
+            })
         }
     },
     { urls: ["https://neal.fun/api/infinite-craft/pair*"] },
