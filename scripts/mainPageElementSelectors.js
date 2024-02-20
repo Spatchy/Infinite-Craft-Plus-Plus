9240806e455167968cbd0050c989f0a06bbfdfad
Spatchy <spatchy@spatchy.net>
2024-02-20 21:44:06 +0000

    Correct instruction label element selector

--- scripts/mainPageElementSelectors.js
+++ scripts/mainPageElementSelectors.js
@@ -1,7 +1,7 @@
 const pageItems = {
   sidebarItems: document.querySelector(".sidebar .items"),
   sidebarControls: document.querySelector(".sidebar .sidebar-controls"),
-  instructionlabel: document.querySelector(".sidebar .items .instruction"),
+  instructionLabel: document.querySelector(".sidebar .items .instruction"),
   sideControls: document.querySelector(".side-controls"),
   particles: document.querySelector(".particles")
 }
